3098bad50edeba48c210d721b3c299d04b24f6c7
lonycn <lonycn@qq.com>
2019-06-06 17:16:45 +0800

    修改app模型

--- app/Models/App.php
+++ app/Models/App.php
@@ -9,13 +9,11 @@ class App extends Model
 {
     const 冷链监测系统 = 'ccrp';
     const 疫苗追溯系统 = 'bpms';
-    protected $fillable = [
-        'name',
-        'appkey',
-        'appsecret',
-        'slug',
-        'image',
-        'note',
-        'status',
-    ];
+    const 疫苗追溯系统演示系统 = 'bpmsdemo';
+    protected $fillable = ['name', 'slug', 'image', 'appkey', 'appsecret', 'status'];
+
+    public function HasUser()
+    {
+        return $this->hasMany(UserHasApp::class);
+    }
 }
