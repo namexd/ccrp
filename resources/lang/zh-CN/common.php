214a19b7b106b6a664cd8e447d0dc4e795246556
lonycn <lonycn@qq.com>
2019-06-27 13:22:15 +0800

    语言翻译文件

---
+++ resources/lang/zh-CN/common.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'id' => 'ID',
+    'name' => '姓名',
+    'username' => '用户名',
+    'password' => '密码',
+    'content' => '内容',
+    'title' => '标题',
+    'email' => '邮箱',
+    'address' => '地址',
+    'created_at' => '创建时间',
+    'updated_at' => '更新时间',
+];
