2e04c3403cc8566db09b19874b4c977facd6fcc1
xiaodi <namexd@163.com>
2019-08-20 18:11:33 +0800

    菜单管理修改

--- app/Http/Controllers/Api/Ccrp/MenusController.php
+++ app/Http/Controllers/Api/Ccrp/MenusController.php
@@ -15,13 +15,17 @@ class MenusController extends Controller
     public function index()
     {
         $this->check();
-        $role= $this->company->cdc_admin?'cdc':'unit';
-        $commonMenu=MenuCommon::pluck('menu_id');
-        $companyMenu=MenuCompany::where('company_id',$this->user->company_id)->pluck('menu_id');
-        $roleMenu=MenuRole::where('role',$role)->pluck('menu_id');
-        $userMenu=MenuUSer::where('status',1)->where('user_id',$this->user->id)->pluck('menu_id');
-        $combine_menus=  array_keys(array_flip($commonMenu)+array_flip($companyMenu)+array_flip($roleMenu)+array_flip($userMenu));
-        $menus=Menu::whereIn('id',$combine_menus)->pluck('slug');
+        $role = $this->company->cdc_admin ? 'cdc' : 'unit';
+        $commonMenu = MenuCommon::pluck('menu_id')->toArray();
+        $companyMenu = MenuCompany::where('company_id', $this->user->company_id)->pluck('menu_id')->toArray();
+        $roleMenu = MenuRole::where('role', $role)->pluck('menu_id')->toArray();
+        $userMenu = MenuUSer::where('status', 1)->where('user_id', $this->user->id)->pluck('menu_id')->toArray();
+        $combine_menus = array_keys(
+            array_flip($commonMenu)
+            + array_flip($companyMenu)
+            + array_flip($roleMenu)
+            + array_flip($userMenu));
+        $menus = Menu::whereIn('id', $combine_menus)->whereRaw('length(slug)>0')->pluck('slug');
         return $this->response->array($menus);
 
     }
